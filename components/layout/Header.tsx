bccda9f62b08b4198a5ba994c3e40d321dac31a8
TimJonkergouw77 <530960@student.fontys.nl>
2026-05-06 11:36:10 +0200

    toevoegen link

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -3,7 +3,7 @@ import StaggeredMenu from "./StaggeredMenu";
 const menuItems = [
   { label: "Home", ariaLabel: "Go to home page", link: "/" },
   { label: "About", ariaLabel: "Learn about us", link: "/#about" },
-  { label: "Services", ariaLabel: "View our services", link: "/#services" },
+  { label: "Services", ariaLabel: "View our services", link: "/fontys-virtual-production" },
   { label: "Process", ariaLabel: "View our process", link: "/#process" },
   { label: "Contact", ariaLabel: "Get in touch", link: "/#contact" },
 ];
